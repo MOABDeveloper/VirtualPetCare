390fb8fe3b7022918704cf6b2eb5c73b4f0b6e31
Aya Abdul-nabi <aabduln@uwo.ca>
2025-03-25 19:29:07 -0400

    load screen

--- LoadScreen.java
+++ LoadScreen.java
@@ -3,48 +3,39 @@ import java.awt.*;
 
 public class LoadScreen extends JLayeredPane {
     private Font customFont;
-    private static CardLayout cardLayoutLoad;
-    private static JPanel mainPanelLoad;
+    private static CardLayout cardLayout;
+    private static JPanel mainPanel;
 
     public LoadScreen(Font customFont) {
         this.customFont = customFont;
         setPreferredSize(new Dimension(1080, 750));
 
+        // background
+        ImageIcon background = new ImageIcon("resources/grid.png");
+        JLabel backgroundLabel = new JLabel(background);
+        backgroundLabel.setBounds(0, 0, 1080, 750);
+        add(backgroundLabel, Integer.valueOf(0));
 
-        // Game screen content
-        JLabel loadText = new JLabel("LOAD");
-        loadText.setFont(customFont);
-        loadText.setForeground(Color.WHITE);
-        Font resizedFont = customFont.deriveFont(Font.PLAIN, 30);
-        loadText.setFont(resizedFont);
-        loadText.setBounds(835, 78, 500, 50);
-        add(loadText, Integer.valueOf(2));
-
-        JButton backButton = MainScreen.buttonCreate(20, 10, 192, 64, "resources/white_button.png", "resources/white_button_clicked.png", "Home");
-        add(backButton, Integer.valueOf(1));
-
-        setVisible(true);
-    }
-
-    private JLayeredPane emptySaves(){
-        JLayeredPane emptySaveScreen = new JLayeredPane();
-        emptySaveScreen.setBounds(0, 0, 1080, 750);
-
-        // load image
-        ImageIcon loadImage = new ImageIcon("resources/save_load_screen.png");
-        Image scaledLoad = loadImage.getImage().getScaledInstance(1080, 750, Image.SCALE_SMOOTH);
+        // actual file thing
+        ImageIcon fileLoad = new ImageIcon("resources/save_load_screen.png");
+        Image scaledLoad = fileLoad.getImage().getScaledInstance(1080, 750, Image.SCALE_SMOOTH);
         ImageIcon scaledLoadIcon = new ImageIcon(scaledLoad);
 
-        // Create background loadText
+        // create label
         JLabel loadLabel = new JLabel(scaledLoadIcon);
         loadLabel.setBounds(0, 0, 1080, 750);
 
-        // Add background to the lowest layer
-        emptySaveScreen.add(loadLabel, Integer.valueOf(2));
+        // add above grid
+        add(loadLabel, Integer.valueOf(1));
 
-        return emptySaveScreen;
+        // back button:
+        JButton homeButton = MainScreen.buttonCreate(20, 10, 192, 64, "resources/white_button.png", "resources/white_button_clicked.png", "Home");
+        add(homeButton, Integer.valueOf(2));
+
+        cardLayout = new CardLayout();
+        mainPanel = new JPanel(cardLayout);
+        mainPanel.setBounds(0, 0, 1080, 750);
+        add(mainPanel, Integer.valueOf(0));
+
     }
 }
-
-
-
